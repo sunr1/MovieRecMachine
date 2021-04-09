b4ddc2cfef427a95d57fa8be5919f8b051e421a5
Connor Waslo <connorwaslo@gmail.com>
2021-04-09 09:21:42 -0500

    Fix cors block error

--- movie-api/server.js
+++ movie-api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes/all');
 
 const app = express()
 const PORT = 8000;
 
+app.use(cors());
 app.use('/', router);
 
 app.listen(PORT, () => {
