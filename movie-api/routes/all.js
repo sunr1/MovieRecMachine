dc852243a9c39028b1c4b2c8a506b688c92051be
Connor Waslo <connorwaslo@gmail.com>
2021-04-22 22:25:16 -0500

    Movie details

--- movie-api/routes/all.js
+++ movie-api/routes/all.js
@@ -4,13 +4,13 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     const offset = req.query.page * 25 || 0;
-    const demoQuery = `
-        SELECT id, title, popularity, release_date, budget, vote_average
+    const query = `
+        SELECT id, title, overview, vote_average, popularity
             FROM movies_metadata
             LIMIT 25 OFFSET ${offset};
     `;
 
-    db.query(demoQuery, (err, result) => {
+    db.query(query, (err, result) => {
         if (err) throw err;
 
         res.send(result);
@@ -30,6 +30,21 @@ router.post('/createList', (req, res) => {
 
         res.send(result);
     });
-})
+});
+
+router.get('/movie', (req, res) => {
+    const id = req.query.id;
+    const query = `
+        SELECT *
+        FROM movies_metadata
+        WHERE id = ${id};
+    `;
+
+    db.query(query, (err, result) => {
+        if (err) throw err;
+
+        res.send(result);
+    });
+});
 
 module.exports = router;
